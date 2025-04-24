70c90d1a22e04f9bae3d19ff6df5d48c53ccc9af
Bychkov Roman <bychkovr51@mail.ru>
2025-04-24 17:30:21 +0300

    all DB

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	//"github.com/gofiber/fiber/v2"
-
 	"fmt"
+	"os"
 )
 
 func main() {
 	var err error
 	if err = Conn_to_DB(); err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 
 }
